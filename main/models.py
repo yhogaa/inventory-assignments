296eca60c9db2bc267195c43661d0b4dd7a8392e
yhogaa <fadrian.yhoga@ui.ac.id>
2023-09-12 00:37:51 +0700

    update for website, add category and some new items

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Items(models.Model):
-    name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
+    item = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
     amount = models.IntegerField()
     description = models.TextField()
