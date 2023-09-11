07699901f0c5da576d6ee24356c854b8d1d97928
yhogaa <fadrian.yhoga@ui.ac.id>
2023-09-11 21:23:26 +0700

    make slight changes to the appearance of the website

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     items = [
-        {'item': 'Air Mineral', 'amount': 10, 'desc': 'biar ga haus'},
-        {'item': 'Biskuit', 'amount': 5, 'desc': 'cemilan ringan'},
-        {'item': 'Buku', 'amount': 3, 'desc': 'hiburan membaca'},
+        {'item': 'Air Mineral', 'amount': 10, 'description': 'biar ga haus'},
+        {'item': 'Biskuit', 'amount': 5, 'description': 'cemilan ringan'},
+        {'item': 'Buku', 'amount': 3, 'description': 'hiburan membaca'},
     ]
 
     context = {
