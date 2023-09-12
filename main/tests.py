b97134d6e2d8629f1c40697b984dc04e05066f3f
yhogaa <fadrian.yhoga@ui.ac.id>
2023-09-13 01:56:41 +0700

    add testing

--- main/tests.py
+++ main/tests.py
@@ -8,3 +8,13 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+     # Test if 'name' exists in context
+    def test_name_is_exist(self):
+        response = Client().get('/main/')
+        self.assertIn('name', response.context)
+
+    # Test if 'class' exists in context
+    def test_class_is_exist(self):
+        response = Client().get('/main/')
+        self.assertIn('class', response.context)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 def show_main(request):
     context = {
         'name' : 'Fadrian Yhoga Pratama',
