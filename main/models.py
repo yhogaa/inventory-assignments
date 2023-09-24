6cedc6a0e146ddc22b80d890948dee01b30569c0
yhogaa <fadrian.yhoga@ui.ac.id>
2023-09-24 22:02:15 +0700

    TUGAS 4

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     category = models.CharField(max_length=256)
     amount = models.IntegerField()
